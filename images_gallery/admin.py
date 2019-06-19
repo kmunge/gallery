536093ce2258070ef44b4268c6590b70d1d94f92
kmunge <orokomunge@gmail.com>
2019-06-19 10:08:35 +0300

    adding admin panel models

--- images_gallery/admin.py
+++ images_gallery/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Location,Category,Image
 
 # Register your models here.
+
 admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Image)
